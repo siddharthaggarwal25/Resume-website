6465850a7b421c611c5fb3ad906a3317ba3ee19b
siddharthaggarwal25 <aggarwalsiddharth49@gmail.com>
2024-11-29 19:04:37 +0530

    some changes

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,23 +6,28 @@ import { projects } from '../../data/constants'
 
 const Projects = () => {
   return (
-    <div style={{ marginTop: "20px" }} >
-    <Container id="projects"   >
-      <Wrapper>
-        <Title>Projects</Title>
-        <Desc>
-          Here are some of my projects.
-        </Desc>
-        <CardContainer>
-          {projects
-            .map((project) => (
-              <ProjectCard project={project} />
+    <div style={{ marginTop: "20px" }}>
+      <Container id="projects">
+        <Wrapper>
+          <Title>Projects</Title>
+          <Desc>Here are some of my projects.</Desc>
+          <CardContainer>
+            {projects.map((project, index) => (
+              <a 
+                key={index} 
+                href={project.github} 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <ProjectCard project={project} />
+              </a>
             ))}
-        </CardContainer>
-      </Wrapper>
-    </Container>
+          </CardContainer>
+        </Wrapper>
+      </Container>
     </div>
-  )
-}
+  );
+};
+
 
 export default Projects
